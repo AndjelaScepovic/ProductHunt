bfd8c501d23e3da49a4975e6bcd71de0e1a6608f
Andjela Scepovic <scepovic.andjela@gmail.com>
2024-02-22 16:53:13 +0100

    adding everything

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -125,8 +125,13 @@ USE_TZ = True
 
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'producthunt/static/')
+    os.path.join(BASE_DIR, 'producthunt/static/'),
+    os.path.join(BASE_DIR, 'products/static/')
+
 ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- products/views.py
+++ products/views.py
@@ -1,4 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Products
+from django.utils import timezone
+
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Products.objects
+    return render(request, 'products/home.html',{'products':products})
+
+@login_required(login_url="/accounts/signup")
+def create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        body = request.POST.get('body')
+        url = request.POST.get('url')
+        icon_file = request.FILES.get('icon')
+        image_file = request.FILES.get('image')
+
+        print("Received title:", title)
+        print("Received body:", body)
+        print("Received URL:", url)
+        print("Received icon file:", icon_file)
+        print("Received image file:", image_file)
+
+
+        if title and body and url and icon_file and image_file:
+            product = Products()
+            product.title = title
+            product.body = body
+            if url.startswith('http://') or url.startswith('https://'):
+                product.url = url
+            else:
+                product.url = 'http://' + url
+            product.icon = icon_file
+            product.image = image_file
+            product.pub_date = timezone.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/' + str(product.id))
+        else:
+            return render(request, 'products/create.html', {'error': 'All fields are required.'})
+    else:
+        return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Products, pk=product_id)
+    return render(request, 'products/detail.html',{'product':product})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Products, pk=product_id)
+        product.votes_total += 1
+        product.save()
+    return redirect('/products/' + str(product.id))
